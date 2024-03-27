c4a8c4f964dd80a9f35db2266638a72308799f25
Tom Lynch <twhlynch@gmail.com>
2024-03-27 17:30:48 +1100

    boosting

--- UltimateTag/main.js
+++ UltimateTag/main.js
@@ -10,6 +10,27 @@ class Object {
             this.y < object.y + object.sy && this.y + this.sy > object.y;
     }
 }
+// 5 second recharge
+class Booster extends Object {
+    constructor(x, y) {
+        super(x, y, 10, 10);
+        this.value = 60;
+        this.wasUsed = 0;
+    }
+    canUse() {
+        return performance.now() - this.wasUsed > 5000;
+    }
+    use() {
+        this.wasUsed = performance.now();
+    }
+    draw() {
+        ctx.fillStyle = 'black';
+        if (this.canUse()) {
+            ctx.fillStyle = 'orange';
+        }
+        ctx.fillRect(this.x, this.y, this.sx, this.sy);
+    }
+}
 class Player extends Object {
     constructor(x, y, sx, sy, speed, color, keys) {
         super(x, y, sx, sy);
@@ -19,6 +40,9 @@ class Player extends Object {
         this.nvy = 0;
         this.speed = speed;
         this.initialSpeed = speed;
+        this.boost = 0;
+        this.maxBoost = 60;
+        this.isBoosting = false;
         this.color = color;
         this.isTagged = false;
         this.wasTagged = 100;
@@ -35,6 +59,12 @@ class Player extends Object {
             } else if (e.key === keys.down) {
                 this.vy = speed;
                 this.nvy = 0;
+            } else if (e.key === keys.activate) {
+                // tbd
+            } else if (e.key === keys.boost) {
+                if (this.boost > 0) {
+                    this.isBoosting = true;
+                }
             }
         });
         document.addEventListener('keyup', (e) => {
@@ -61,14 +91,26 @@ class Player extends Object {
         this.isTagged = false;
         this.speed = this.initialSpeed;
     }
+    addBoost(boost) {
+        this.boost = Math.min(this.boost + boost, this.maxBoost)
+    }
     move() {
         const vx = this.vx - this.nvx;
         const vy = this.vy - this.nvy;
         const speed = Math.sqrt(vx * vx + vy * vy);
-        if (speed > 0) {
-            this.x += vx / speed * this.speed;
-            this.y += vy / speed * this.speed;
+        let boost = 1;
+        if (this.isBoosting && this.boost > 0) {
+            this.boost--;
+            boost = 1.5;
+            if (this.boost == 0) {
+                this.isBoosting = false;
+            }
         }
+        if (speed > 0) {
+            this.x += vx / speed * this.speed * boost;
+            this.y += vy / speed * this.speed * boost;
+        }
+
         
         if (this.x < 0) {
             this.x = 0;
@@ -101,9 +143,18 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
 
-const player1 = new Player(canvas.width / 2, canvas.height / 2, 10, 10, 5, 'blue', { left: 'ArrowLeft', right: 'ArrowRight', up: 'ArrowUp', down: 'ArrowDown' });
-const player2 = new Player(canvas.width / 2, canvas.height / 2, 10, 10, 5, 'green', { left: 'a', right: 'd', up: 'w', down: 's' });
+const player1 = new Player(canvas.width / 4, canvas.height / 2, 10, 10, 3, 'blue', { left: 'a', right: 'd', up: 'w', down: 's', activate: ' ', boost: 'e' });
+const player2 = new Player(canvas.width / 4 * 3, canvas.height / 2, 10, 10, 3, 'green', { left: 'ArrowLeft', right: 'ArrowRight', up: 'ArrowUp', down: 'ArrowDown', activate: 'Enter', boost: 'Shift'});
 player1.tag();
+
+const boosters = [];
+for (let x = 0; x < 3; x++) {
+    for (let y = 0; y < 3; y++) {
+        const thirdX = canvas.width / 3 * x + canvas.width / 6;
+        const thirdY = canvas.height / 3 * y + canvas.height / 6;
+        boosters.push(new Booster(thirdX, thirdY));
+    }
+}
 
 let lastRender = performance.now();
 let frameCount = 0;
@@ -137,8 +188,25 @@ function render() {
         }
     }
 
+    boosters.forEach(booster => {
+        if (booster.canUse()) {
+            if (booster.isColliding(player1)) {
+                booster.use();
+                player1.addBoost(booster.value);
+            }
+            if (booster.isColliding(player2)) {
+                booster.use();
+                player2.addBoost(booster.value);
+            }
+        }
+    });
+
     // Draw Scene
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    boosters.forEach(booster => {
+        booster.draw();
+    });
 
     player1.draw();
     player2.draw();
